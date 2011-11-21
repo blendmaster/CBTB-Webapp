eccc43ae5f014de7adb5d7014ec3391d56353d0e
Steven Ruppert <blendmaster@gmail.com>
2011-11-20 22:35:06 -0700

    made register.php act properly

--- register.php
+++ register.php
@@ -25,15 +25,28 @@
 	</header>
 	<div id="main" role="main">
 		<form>
-			<label for="username">Username</label><input type="text" name="username" id="username" />
-			<label for="email">Email</label><input type="text" name="email" id="email" />
-			<label for="password">Password</label><input type="password" name="password" id="password" />
-			<label for="confirmPassword">Password Confirm</label><input type="password" name="confirmPassword" id="confirmPassword" />
-			<label for="organization">Organization</label><select name="organization" id="organization">
-				<option value="organizationAlpha">Organization Alpha</option>
-				<option value="organizationOmega">Organization Omega</option>
-			</select>
-			<input type="Submit" value="Create Account" />
+			<p>
+				<label for="username">Username</label><input type="text" name="username" id="username" />
+			</p>
+			<p>
+				<label for="email">Email</label><input type="email" name="email" id="email" />
+			</p>
+			<p>
+				<label for="password">Password</label><input type="password" name="password" id="password" />
+			</p>
+			<p>
+				<label for="confirmPassword">Password Confirm</label><input type="password" name="confirmPassword" id="confirmPassword" />
+			</p>
+			<p>
+				<label for="organization">Organization</label>
+				<select name="organization" id="organization">
+					<option value="organizationAlpha">Organization Alpha</option>
+					<option value="organizationOmega">Organization Omega</option>\
+				</select>
+			</p>
+			<p>
+				<input type="Submit" value="Create Account" />
+			</p>
 		</form>
 	</div>
 	<footer>
