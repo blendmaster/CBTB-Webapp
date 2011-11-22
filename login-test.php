aa55220a486c83a3d8bfc01e92d5a42ae702ef06
William Weiskopf <worldwidewilly@comcast.net>
2011-11-22 00:07:42 -0700

    Correcting redirects

--- login-test.php
+++ login-test.php
@@ -10,9 +10,7 @@
   $sql="SELECT * FROM users WHERE username ='$myusername' and password='$mypassword'";
   $result=mysql_query($sql);
 
-  // Mysql_num_row is counting table row
   $count=mysql_num_rows($result);
-  // If result matched $myusername and $mypassword, table row must be 1 row
 
   if($count==1) {
     session_register("myusername");
