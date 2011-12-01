f3a8a868d1bbea03a266f2859841108f11ea1eb7
William Weiskopf <worldwidewilly@comcast.net>
2011-12-01 01:11:51 -0700

    Get rid of seperate branch

--- adddonation.php
+++ adddonation.php
@@ -1,3 +1,40 @@
+<?php
+	ob_start(); #output buffer for redirect
+	require_once "includes/db.inc.php";
+	require_once "includes/PasswordHash.php";
+	require_once "includes/session.inc.php";
+	
+	$error = false;
+	
+	if( count($_POST) > 0 and 
+	  $username = $_POST['username'] and
+		$password = $_POST['password'] ) {
+
+		if ($dbh = open_db() ) {
+			try { 
+				$users = $dbh->prepare('select id, username, password from users where username = :username');
+				$users->execute(array(":username" => $username));
+				
+				if($user = $users->fetch()) {
+					if( password_check($password, $user['password']) ) {
+						log_in($user);
+						header("Location: http://{$_SERVER['SERVER_NAME']}/team13/dashboard.php"); 
+						exit();
+					} else {
+						$error = "Incorrect password for user \"$username\"";
+					}
+				} else {
+					$error = "user \"$username\" doesn't exist.";
+				}
+			} catch (PDOException $e) {
+				$error = 'Connection failed: ' . $e->getMessage();
+			}	
+		} else {
+			$error = "Error connecting to db";
+		}
+	}
+?>
+
 <!doctype html>
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
