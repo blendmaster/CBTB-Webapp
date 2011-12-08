720bda51f5f27ae9a96d15dfc0332d5a0ab71c1b
Jonathan Tran <Jonathan.Chi.Tran@gmail.com>
2011-12-08 02:35:51 -0700

    ARRGH

--- js/inventorySort.js
+++ js/inventorySort.js
@@ -1,7 +1,6 @@
-$(document).ready(function() { 
-    // call the tablesorter plugin 
-    $("table").tablesorter({ 
-        // sort on the first column and third column, order asc 
-        sortList: [[0,0],[2,0]] 
-    }); 
-}); 
+$(document).ready(function() 
+    { 
+		// call the tablesorter plugin 
+        $("#inventoryTable").tablesorter(); 
+    } 
+); 
