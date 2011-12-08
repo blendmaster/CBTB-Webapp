60ea59361c3fb1b1762710d3b69e5d736fb99e7c
Jonathan Tran <Jonathan.Chi.Tran@gmail.com>
2011-12-08 02:29:40 -0700

    Work you!!!

--- inventory.php
+++ inventory.php
@@ -7,7 +7,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<?php include "includes/headmatter.inc.php" ?>
-	<script type="text/javascript" src="js/libs/inventorySort.js></script>
+	<script type="text/javascript" src="js/libs/inventorySort.js"></script>
 	<title>Compassion by the Book</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
